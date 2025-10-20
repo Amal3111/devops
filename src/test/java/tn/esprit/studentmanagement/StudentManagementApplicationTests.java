2ce3fa9899b7b73cc25a12442b338b66dfcee15a
Amal3111 <amalbouslimi51@gmail.com>
2025-10-20 17:33:49 +0100

    Initial commit with logs and tests2

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,20 +1,27 @@
 package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import tn.esprit.studentmanagement.entities.Student;
 import tn.esprit.studentmanagement.services.StudentService;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
+@Sql(scripts = "/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class StudentManagementApplicationTests {
     @Autowired
     private StudentService studentService;
-     @Test
+
+    @Test
     public void testGetAllStudents() {
         List<Student> students = studentService.getAllStudents();
         assertThat(students).isNotEmpty();
     }
+
     @Test
     void contextLoads() {
     }
-
 }
