5b8854e8999c9b689a9b6f9751313bd5d2601599
Amal3111 <amalbouslimi51@gmail.com>
2025-10-20 17:23:35 +0100

    Initial commit with logs and tests

--- src/main/java/tn/esprit/studentmanagement/services/StudentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/StudentService.java
@@ -5,13 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.entities.Student;
 import tn.esprit.studentmanagement.repositories.StudentRepository;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.List;
 
 @Service
 public class StudentService implements IStudentService {
+     private static final Logger logger = LogManager.getLogger(StudentService.class);
+  
     @Autowired
     private StudentRepository studentRepository;
+      public void testMethod() {
+        logger.info("Info log");
+        logger.debug("Debug log");
+        logger.error("Error log");
+    }
     public List<Student> getAllStudents() { return studentRepository.findAll(); }
     public Student getStudentById(Long id) { return studentRepository.findById(id).orElse(null); }
     public Student saveStudent(Student student) { return studentRepository.save(student); }
--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -5,7 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudentManagementApplicationTests {
-
+    @Autowired
+    private StudentService studentService;
+     @Test
+    public void testGetAllStudents() {
+        List<Student> students = studentService.getAllStudents();
+        assertThat(students).isNotEmpty();
+    }
     @Test
     void contextLoads() {
     }
